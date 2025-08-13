d2ba143746a42eddf5c54da7bb18f932665d9a84
anliberant <anliberant@gmail.com>
2025-08-13 04:15:39 +0300

    eslint fixed

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,15 +7,6 @@ import globals from 'globals';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 
-import { FlatCompat } from '@eslint/eslintrc';
-import js from '@eslint/js';
-import tsParser from '@typescript-eslint/parser';
-import parser from 'astro-eslint-parser';
-import { defineConfig, globalIgnores } from 'eslint/config';
-import globals from 'globals';
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
